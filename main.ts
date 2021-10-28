3937d0f0b3cfedad625d2b5fb60449603bfc7350
Google0927 <72186353+Google0927@users.noreply.github.com>
2021-10-28 11:16:24 +0900

    Aボタンの挙動

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
+function A_function () {
+    basic.showString("A")
     basic.clearScreen()
     basic.showArrow(ArrowNames.North)
-})
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P0, 0)
+}
